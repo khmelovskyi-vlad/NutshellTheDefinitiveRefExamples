afa25c51dae7ac6b326650f590c0007946b41b2a
stone1234765 <51057074+stone1234765@users.noreply.github.com>
2021-11-18 16:04:08 +0200

    Add project files.

---
+++ NutshellTheDefinitiveRefExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NutshellTheDefinitiveRefExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
